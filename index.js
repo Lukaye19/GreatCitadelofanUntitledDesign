704c673bc6c1ff84ca08b9a69b8dca04de457520
Lukaye19 <roluka19@gmail.com>
2025-10-18 09:52:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/stockfish", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Chess API proxy running on port ${PORT}`);
 });
